c88b41cb16b092a512f07cb02d7038698ddb4c11
Blake Alderink <blake.alderink@gmail.com>
2023-03-21 12:48:16 -0700

    set up server, added posts router and get endpoints for posts

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,40 @@
+const express = require('express')
+const Posts = require('./posts-model')
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    Posts.find()
+    .then(posts => {
+        res.status(200).json(posts)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: "Error: posts not found"
+        })
+    })
+    
+})
+
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    let returnPost;
+    Posts.findById(id)
+    .then(post => {
+        returnPost = post;
+    })
+    .then(() => {if (!returnPost) {
+res.status(404).json({
+    message: "this id is not a real post"
+})
+    }
+    else {
+        res.status(200).json(returnPost)
+    }}
+    
+    
+    )
+})
+
+module.exports = router;
 // implement your posts router here
--- api/server.js
+++ api/server.js
@@ -1,2 +1,20 @@
+const express = require('express')
+
+const postRoutes = require('./posts/posts-router')
+
+
+const server = express()
+
+server.use('/api/posts', postRoutes)
+server.use(express.json())
+
+server.use('*', (req, res) => {
+    res.status(404).json({
+        message: "not found"
+    })
+})
+
+
+module.exports = server;
 // implement your server here
 // require your posts router and connect it here
